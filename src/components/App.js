7a9a9e643994285e4ef720f03d49eaa2e0aa3241
thepedroferrari <pedro@pedroferrari.com>
2018-06-24 20:33:40 +0200

    Displaying State with JSX

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
   state = {
@@ -31,6 +32,11 @@ class App extends React.Component {
         <div className="catch-of-the-day">
           <nav className="menu">
             <Header tagline="Fresh Seafood Market" />
+            <ul className="fishes">
+              {Object.keys(this.state.fishes).map((key) => (
+              <Fish key={key} details={this.state.fishes[key]} />
+              ))}
+            </ul>
           </nav>
           <Order />
           <Inventory
---
+++ src/components/Fish.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { formatPrice } from '../helpers';
+
+class Fish extends React.Component {
+  render() {
+    // ES6 Destructuring
+    const { image, name, price, status, desc } = this.props.details;
+    return (
+      <li className="menu-fish">
+        <img src={image} alt={name} />
+        <h3 className="fish-name">
+          {name}
+          <span className="price">{formatPrice(price)}</span>
+        </h3>
+        <p>{desc}</p>
+        <button>Add To Cart</button>
+      </li>
+    );
+  }
+}
+
+export default Fish;
