f8bf4e085ff90e96da3f0f59c85d8fb47a419fc5
thepedroferrari <pedro@pedroferrari.com>
2018-06-25 22:05:14 +0200

    Displaying Order State with JSX

--- src/components/App.js
+++ src/components/App.js
@@ -29,14 +29,14 @@ class App extends React.Component {
 
   addToOrder = (key) => {
     // 1. take a copy of state
-    const order = { ...this.state.order }
+    const order = { ...this.state.order };
 
     // 2. Either add to the order, or update the number in our order
-    order[key] = order[key] + 1 || 1
+    order[key] = order[key] + 1 || 1;
 
     // 3. Call setState to update our state object
     this.setState({ order });
-  }
+  };
 
   render() {
     return (
@@ -46,11 +46,16 @@ class App extends React.Component {
             <Header tagline="Fresh Seafood Market" />
             <ul className="fishes">
               {Object.keys(this.state.fishes).map((key) => (
-              <Fish key={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} index={key} />
+                <Fish
+                  key={key}
+                  details={this.state.fishes[key]}
+                  addToOrder={this.addToOrder}
+                  index={key}
+                />
               ))}
             </ul>
           </nav>
-          <Order />
+          <Order fishes={this.state.fishes} order={this.state.order} />
           <Inventory
             addFish={this.addFish}
             loadSampleFishes={this.loadSampleFishes}
